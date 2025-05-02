8401b7bc197af4107dbe2166eaa5b4f5f3a254bc
Minhdev2004 <chinhtpc123@gmail.com>
2025-05-02 11:05:23 +0700

    update header

--- web_project/views/Template/Header.php
+++ web_project/views/Template/Header.php
@@ -51,10 +51,10 @@
 
 <!-- Thanh navbar chính -->
 <div class="bg-red-800 text-white ">
-  <div class="container mx-auto px-4 py-4 flex items-center justify-between w-xl">
+  <div class=" w-3/4 mx-auto py-4 flex items-center justify-between">
     
-    <!-- Logo MUJI -->
-    <div class="flex-shrink-0 pr-4">
+    <!-- Logo App -->
+    <div class="flex-shrink-0 pl-3 ">
       <h1 class="text-3xl font-bold leading-tight">
       <?="".($GLOBALS['site_info']['Company_name'] ?? 'Chưa có tên') ?>
         
@@ -74,16 +74,25 @@
     </ul>
 
     <!-- Tìm kiếm & Giỏ hàng -->
-    <div class="flex items-center space-x-4 w-1/3">
+    <div class="flex items-center space-x-4 ">
       
-      <div class="relative flex-grow">
-        <input type="text" placeholder="Bạn đang muốn tìm kiếm gì?" class="w-full rounded-full pl-10 pr-10 py-2 text-sm text-gray-700 focus:outline-none" />
-        <span class="absolute left-3 top-2.5 text-gray-400">🔍</span>
-        <span class="absolute right-3 top-2.5 text-gray-400 cursor-pointer">❌</span>
+      <!-- search -->
        
-      </div>
+      <form class="max-w-md mx-auto">   
+          <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            </div>
+      </form>
 
-      <a href="#" class="text-white text-xl">🛒</a>
+
+      
     </div>
   </div>
 </div>
